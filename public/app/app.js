c71ef90b16688ee7830f924e6f2d79ef12be727c
Justin Rassier <justinrassier@outlook.com>
2014-03-03 14:13:19 -0600

    Admin page of UI

--- public/app/app.js
+++ public/app/app.js
@@ -5,6 +5,7 @@ app.config(function($routeProvider, $locationProvider){
     $routeProvider
         .when('/', {templateUrl: '/partials/main/main', controller: 'mvMainCtrl'})
         .when('/recipe', {templateUrl: '/partials/recipe/browseRecipes', controller: 'browseRecipe'})
+        .when('/admin/users', {templateUrl: '/partials/admin/user-list', controller: 'mvUserListCtrl'})
 
 });
 

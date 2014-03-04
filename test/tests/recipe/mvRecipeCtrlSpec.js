1056cc87be56e8530c6b42e5da54772bb86387fb
Justin Rassier <justinrassier@outlook.com>
2014-03-04 16:00:56 -0600

    Refactored and added some basic tests

--- test/tests/recipe/mvRecipeCtrlSpec.js
+++ test/tests/recipe/mvRecipeCtrlSpec.js
@@ -2,12 +2,21 @@ describe('mvRecipeCtrl', function(){
     beforeEach(module('app'));
 
     describe('The Controller',function(){
-        it('Should Contain Routes', inject(function($rootScope,$controller){
-            var scope = $rootScope.$new(),
-                ctrl = $controller('browseRecipe', {$scope: scope});
+        var scope,
+            ctrl;
+
+        it('Should Contain 4 Routes', inject(function($rootScope,$controller){
+                 scope = $rootScope.$new();
+                 ctrl = $controller('browseRecipe', {$scope: scope});
 
                 expect(scope.recipes.length).toBe(4);
+        }));
 
+        it('Should contains a route of "IPA" as the first route', inject(function($rootScope, $controller){
+                scope = $rootScope.$new();
+                ctrl = $controller('browseRecipe', {$scope: scope});
+
+            expect(scope.recipes[0].name).toBe('IPA');
         }));
 
     });

8b2993772f032f8b7a87ac8628e0ca2b7fe2e372
Justin Rassier <justinrassier@outlook.com>
2014-03-05 22:26:04 -0600

    Added cached courses resource

--- public/app/main/mvMainCtrl.js
+++ public/app/main/mvMainCtrl.js
@@ -1,5 +1,5 @@
 //prove angular app is working
-angular.module('app').controller('mvMainCtrl',function($scope, mvCourse){
-    $scope.courses =  mvCourse.query();
+angular.module('app').controller('mvMainCtrl',function($scope, mvCachedCourses){
+    $scope.courses =  mvCachedCourses.query();
 });
 

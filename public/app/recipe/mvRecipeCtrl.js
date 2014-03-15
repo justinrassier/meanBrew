5edc966e2ad3369afc30da3efcfa147fc14a33c0
Justin Rassier <justinrassier@outlook.com>
2014-03-15 08:24:48 -0500

    Added navigation to create recipe

--- public/app/recipe/mvRecipeCtrl.js
+++ public/app/recipe/mvRecipeCtrl.js
@@ -1,7 +1,4 @@
-angular.module('app').controller('mvRecipeCtrl', function($scope,$location, mvIdentity){
+angular.module('app').controller('mvRecipeCtrl', function($scope, mvIdentity){
     //right now the whole user object graph is getting sent up, so just attach the recipes to the scope
     $scope.myRecipes = mvIdentity.currentUser.recipes;
-    $scope.goToCreate = function(){
-        $location.path('/recipe/create');
-    };
 });

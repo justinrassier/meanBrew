5566288090b13edc24ebf2c3d0af401ac912d9e3
Justin Rassier <justinrassier@outlook.com>
2014-03-18 14:23:10 -0500

    Removed auto login for testing

--- public/app/main/mvMainCtrl.js
+++ public/app/main/mvMainCtrl.js
@@ -1,5 +1,5 @@
 //prove angular app is working
 angular.module('app').controller('mvMainCtrl',function($scope, mvAuth){
-    mvAuth.authenticateUser('justinrassier@outlook.com','justin');
+    
 });
 
